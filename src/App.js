ea6d46ae9a300ccc4efbe7547aece4fcd48759c7
Alvisalc <alvisccy1107@gmail.com>
2023-12-06 21:48:46 -0500

    router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Navbar } from './Components/Navbar/Navbar';
+import { Shop } from './Pages/Shop';
+import { Category } from './Pages/Category';
+import { Product } from './Pages/Product';
+import { SignUp } from './Pages/SignUp';
+import { Cart } from './Pages/Cart';
 
 function App() {
   return (
     <div>
-     <Navbar/>
+      <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Shop/>}/>
+        <Route path='/men' element={<Category category="men"/>}/>
+        <Route path='/women' element={<Category category="women"/>}/>
+        <Route path='/kids' element={<Category category="kids"/>}/>
+        <Route path='/product' element={<Product/>}>
+          <Route path=':productId' element={<Product/>}/>
+        </Route>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/login' element={<SignUp/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
