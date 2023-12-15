80eb4ca4ca29086d5f24ba84bbea0dee0796b294
Alvisalc <alvisccy1107@gmail.com>
2023-12-15 13:54:19 -0500

    props

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Navbar } from './Components/Navbar/Navbar';
 import { Shop } from './Pages/Shop';
-import { Category } from './Pages/Category';
+import  Category  from './Pages/Category';
 import { Product } from './Pages/Product';
 import { SignUp } from './Pages/SignUp';
 import { Cart } from './Pages/Cart';
 import { Footer } from './Components/Footer/Footer';
+import  men_banner from './Components/Assets/banner_mens.png'
+import  women_banner from './Components/Assets/banner_women.png'
+import  kids_banner from './Components/Assets/banner_kids.png'
 
 function App() {
   return (
@@ -15,9 +18,9 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path='/' element={<Shop/>}/>
-        <Route path='/men' element={<Category category="men"/>}/>
-        <Route path='/women' element={<Category category="women"/>}/>
-        <Route path='/kids' element={<Category category="kids"/>}/>
+        <Route path='/men' element={<Category banner={men_banner} category="men"/>}/>
+        <Route path='/women' element={<Category banner={women_banner} category="women"/>}/>
+        <Route path='/kids' element={<Category banner={kids_banner} category="kids"/>}/>
         <Route path='/product' element={<Product/>}>
           <Route path=':productId' element={<Product/>}/>
         </Route>
--- src/Pages/Category.jsx
+++ src/Pages/Category.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import './CSS/Category.css'
+import ShopContext from '../Context/ShopContext'
+import dropdown_icon from '../Components/Assets/dropdown_icon.png'
 
-export const Category = () => {
+const Category = (props) => {
+
+  useContext(ShopContext);
+
   return (
-    <div>Category</div>
+    <div className="shop-category">
+      <img src={props.banner} alt="" />
+      <div className="shopcategory-indexSort">
+        <p>
+          <span>Showing 1-12</span> out of 36 products
+        </p>
+        <div className="shopcategory-sort">
+          Sort by <img src={dropdown_icon} alt="" />
+        </div>
+      </div>
+    </div>
   )
 }
+
+export default Category
